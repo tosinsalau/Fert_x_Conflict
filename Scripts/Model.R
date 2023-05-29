c87a3792b81db82f4f71ab8fb3885be06a8d840e
tosinsalau <tosin.salau01@gmail.com>
2023-05-28 22:05:35 -0400

    initial commit

---
+++ Scripts/Model.R
@@ -0,0 +1,27 @@
+library(readr)
+library(dplyr)
+library(modelsummary)
+library(MASS)
+pth = 'C:/Users/salau/OneDrive - Michigan State University/Research/Fert_x_Conflict/All_Data/'
+
+load(paste0(pth, 'dat.rda'))
+
+#descriptive stats
+datasummary_skim(dat)
+
+#model
+mod= glm.nb(total_collab ~ Subsidy_price1 + log(GDP_per_capita)+
+              log(total_pop)+ log(rural_pop) + rur_tot_pop + part_dem +
+              opp_free + elect_dem +
+              factor(year) +factor(cname), data = dat)
+summary(mod)
+
+
+mod= glm.nb(total_collab ~ Subsidy_price1 + log(GDP_per_capita)+ civ_war + urb_tot_pop + pop_0_14 +
+              gdp_growth + life_exp + mort_rate + pop_growth +
+              log(total_pop)+ log(rural_pop) + rur_tot_pop + part_dem +
+              opp_free + elect_dem +
+              factor(year) +factor(cname), data = dat)
+summary(mod)
+
+                 
