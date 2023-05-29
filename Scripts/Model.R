27d7dcf1f25b26dc42cc4100e629c86e5bc8cebe
tosinsalau <tosin.salau01@gmail.com>
2023-05-29 18:18:48 -0400

    Update Model.R

--- Scripts/Model.R
+++ Scripts/Model.R
@@ -2,6 +2,7 @@ library(readr)
 library(dplyr)
 library(modelsummary)
 library(MASS)
+library(pscl)
 pth = 'C:/Users/salau/OneDrive - Michigan State University/Research/Fert_x_Conflict/All_Data/'
 
 load(paste0(pth, 'dat.rda'))
@@ -9,9 +10,14 @@ load(paste0(pth, 'dat.rda'))
 #descriptive stats
 datasummary_skim(dat)
 
+plot(dat$Subsidy_price1, dat$total_collab)
+hist(dat$Subsidy_price1)
+hist(dat$total_collab)
+table(dat$total_collab)
+cor(dat$Subsidy_price1, dat$total_collab)
+
+
 #model
-
-
 mod= glm.nb(total_collab ~ Subsidy_price1 + log(GDP_per_capita)+
               log(total_pop)+ log(rural_pop) + rur_tot_pop + part_dem +
               opp_free + elect_dem +
@@ -19,12 +25,12 @@ mod= glm.nb(total_collab ~ Subsidy_price1 + log(GDP_per_capita)+
 summary(mod)
 
 
-mod= glm.nb(total_collab ~ Subsidy_price1 + log(GDP_per_capita)+ civ_war + urb_tot_pop + pop_0_14 +
+mod1= glm.nb(total_collab ~ Subsidy_price1 + log(GDP_per_capita)+ civ_war + urb_tot_pop + pop_0_14 +
               gdp_growth + life_exp + mort_rate + pop_growth +
               log(total_pop)+ log(rural_pop) + rur_tot_pop + part_dem +
-              opp_free + elect_dem + polity +
+              opp_free + elect_dem + polity2 +
               factor(year) +factor(cname), data = dat)
-summary(mod)
+summary(mod1)
 
 
 zpoisMod = zeroinfl(total_collab ~ Subsidy_price1 + log(GDP_per_capita)+
